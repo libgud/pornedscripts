120f87e13273c1f8c5072ee5345da77f35eefbd9
libgud <gudassrtasak@gmail.com>
2025-09-29 22:21:09 +0300

    Add files via upload

---
+++ aivibe.js
@@ -0,0 +1,444 @@
+//AI SHITCODE
+
+;)()}
+}    
+;)sm_llop ,}        
+}            
+;)esab(esab_htiw_partstoob                
+;)llop(lavretnIraelc                
+{ )esab( fi            
+;)(esab_bil_teg = esab            
+{ )( noitcnuf(lavretnItes = llop rav        
+{ esle }    
+;)esab(esab_htiw_partstoob        
+{ )esab( fi    
+;)(esab_bil_teg = esab rav    
+
+}    
+;)(nottub_tixe_etaerc = nottub_tixe        
+
+;}        
+}            
+}                
+;eslaf nruter                    
+{ )e( hctac }                
+;eslaf nruter                    
+}                    
+;eurt nruter                        
+;][ = sriap_nottub_launam                        
+;llun = pupop_tnerruc                        
+;)}                        
+}{ )e( hctac }                            
+}                                
+;)0(8Uetirw.)87(dda.ntb.neerg.riap                                    
+{ ))(lluNsi.)ntb.neerg.riap(rtp! && ntb.neerg.riap && neerg.riap( fi                                
+}                                
+;)0(8Uetirw.)87(dda.ntb.lamron.riap                                    
+{ ))(lluNsi.)ntb.lamron.riap(rtp! && ntb.lamron.riap && lamron.riap( fi                                
+{ yrt                            
+{ >= riap(hcaErof.sriap_nottub_launam                        
+}{ )e( hctac }                        
+;)y_neercs_ffo ,x_neercs_ffo ,pupop_tnerruc(noitisop_tes_efas                            
+{ yrt                        
+}{ )e( hctac }                        
+;)0(8Uetirw.)87(dda.pupop_tnerruc                            
+{ yrt                        
+{ ))(lluNsi.)pupop_tnerruc(rtp! && pupop_tnerruc( fi                    
+{ yrt                
+{ )( noitcnuf :pupopesolc            
+,}            
+;llun : )(gnirtSot.)p(rtp ? p nruter                
+;)(pupop_pirts_etaerc = p rav                
+}                
+;)(gnirtSot.)pupop_tnerruc(rtp nruter                    
+{ ))(lluNsi.)pupop_tnerruc(rtp! && pupop_tnerruc( fi                
+{ )( noitcnuf :pupopnwaps            
+{ = stropxe.cpr        
+
+}{ )e( hctac }        
+;)}            
+}                
+}{ )e( hctac }                    
+}                        
+}                            
+;nruter                                
+;'lamron' = ]txet.riap[setats_nottub                                
+;'lamron' = etats.riap                                
+;)y_sop.riap ,x_sop.riap ,ntb.lamron.riap(noitisop_tes_efas                                
+;)y_neercs_ffo ,x_neercs_ffo ,ntb.neerg.riap(noitisop_tes_efas                                
+{ )rts_p === )(gnirtSot.ntb.neerg.riap && )(lluNsi.)ntb.neerg.riap(rtp! && ntb.neerg.riap && neerg.riap( fi esle }                            
+;nruter                                
+;'neerg' = ]txet.riap[setats_nottub                                
+;'neerg' = etats.riap                                
+;)y_sop.riap ,x_sop.riap ,ntb.neerg.riap(noitisop_tes_efas                                
+;)y_neercs_ffo ,x_neercs_ffo ,ntb.lamron.riap(noitisop_tes_efas                                
+{ )rts_p === )(gnirtSot.ntb.lamron.riap && )(lluNsi.)ntb.lamron.riap(rtp! && ntb.lamron.riap && lamron.riap( fi                            
+;]i[sriap_nottub_launam = riap rav                            
+{ )++i ;htgnel.sriap_nottub_launam < i ;0 = i rav( rof                        
+;)(gnirtSot.rtp_desserp = rts_p rav                        
+
+}                        
+}                            
+;nruter                                
+;)(pupopesolc.stropxe.cpr                                
+{ ))(gnirtSot.rtp_ntb_esolc === )(gnirtSot.rtp_desserp && )(lluNsi.)rtp_ntb_esolc(rtp! && rtp_ntb_esolc( fi                            
+}{ )e( hctac }                            
+;)(retnioPdaer.)632(dda.pupop_tnerruc = rtp_ntb_esolc                                
+{ yrt                            
+;llun = rtp_ntb_esolc rav                            
+{ ))(lluNsi.)pupop_tnerruc(rtp! && pupop_tnerruc( fi                        
+
+}                        
+;nruter                            
+;)(pupopnwaps.stropxe.cpr                            
+{ ))(gnirtSot.nottub_tixe === )(gnirtSot.rtp_desserp && )(lluNsi.)nottub_tixe(rtp! && nottub_tixe( fi                        
+
+;)(gnirtSot.rtp_desserp = nottub_desserp_tsal                        
+;emit_tnerruc = emit_sserp_tsal                        
+}                        
+;nruter                            
+{ ))(gnirtSot.rtp_desserp === nottub_desserp_tsal && sm_ecnuobed < emit_sserp_tsal - emit_tnerruc( fi                        
+;)(won.etaD = emit_tnerruc rav                        
+
+}                        
+;nruter                            
+{ ))(lluNsi.)rtp_desserp(rtp || rtp_desserp!( fi                        
+;]0[sgra = rtp_desserp rav                        
+{ yrt                    
+{ )sgra( noitcnuf :retnEno                
+{ ,nf_nottubmotsuc(hcatta.rotpecretnI            
+;)]'retniop'[ ,'diov' ,)desserp_nottubmotsuc_ffo(dda.esab(noitcnuFevitaN wen = nf_nottubmotsuc rav            
+{ yrt        
+
+;llun = nottub_desserp_tsal rav        
+;0 = emit_sserp_tsal rav        
+
+}        
+}            
+;llun nruter                
+{ )e( hctac }            
+;jbo nruter                
+;jbo = pupop_tnerruc                
+
+}                
+}{ )e( hctac }                    
+}                        
+}                            
+;'neerg' = ]txet.gfc[setats_nottub                                
+{ )egap_txen_si( fi                            
+;)} etats :etats ,y.gfc :y_sop ,x.gfc :x_sop ,txet.gfc :txet ,neerg :neerg ,lamron :lamron {(hsup.sriap_nottub_launam                            
+}                            
+;)y_neercs_ffo ,x_neercs_ffo ,ntb.neerg(noitisop_tes_efas                                
+;)y.gfc ,x.gfc ,ntb.lamron(noitisop_tes_efas                                
+{ esle }                            
+;)y.gfc ,x.gfc ,ntb.neerg(noitisop_tes_efas                                
+;)y_neercs_ffo ,x_neercs_ffo ,ntb.lamron(noitisop_tes_efas                                
+{ )'neerg' === etats( fi                            
+;)'lamron' || ]txet.gfc[setats_nottub( : 'neerg' ? egap_txen_si = etats rav                            
+;"egap txen" === txet.gfc = egap_txen_si rav                            
+{ )neerg && lamron( fi                        
+;)2 ,w.gfc ,h.gfc ,y_neercs_ffo ,x_neercs_ffo ,txet.gfc ,"meti_yrtnuoc"(nottub_launam_etaerc = neerg rav                        
+;)1 ,w.gfc ,h.gfc ,y.gfc ,x.gfc ,txet.gfc ,"meti_yrtnuoc"(nottub_launam_etaerc = lamron rav                        
+{ yrt                    
+;]i[sgifnoc_nottub = gfc rav                    
+{ )++i ;htgnel.sgifnoc_nottub < i ;0 = i rav( rof                
+
+;]                
+} 1.1 :w ,1.1 :h ,024 :y ,275 :x ,"domst/gg.drocsid" :txet {                    
+,} 58.0 :w ,58.0 :h ,004 :y ,397 :x ,"egap txen" :txet {                    
+,} 58.0 :w ,58.0 :h ,004 :y ,853 :x ,"egap suoiverp" :txet {                    
+,} 58.0 :w ,58.0 :h ,503 :y ,397 :x ,"regnahc niks" :txet {                    
+,} 58.0 :w ,58.0 :h ,503 :y ,846 :x ,"regnahc emeht" :txet {                    
+,} 58.0 :w ,58.0 :h ,503 :y ,305 :x ,"metsys knar dlo" :txet {                    
+,} 58.0 :w ,58.0 :h ,503 :y ,853 :x ,"dom itlu inan" :txet {                    
+,} 58.0 :w ,58.0 :h ,052 :y ,397 :x ,"dom deknar" :txet {                    
+,} 58.0 :w ,58.0 :h ,052 :y ,846 :x ,"setome lla kcolnu" :txet {                    
+,} 58.0 :w ,58.0 :h ,052 :y ,305 :x ,"eman citamorhc" :txet {                    
+,} 58.0 :w ,58.0 :h ,052 :y ,853 :x ,"regnahc revres elttab" :txet {                    
+,} 58.0 :w ,58.0 :h ,591 :y ,397 :x ,"egrahc otua" :txet {                    
+,} 58.0 :w ,58.0 :h ,591 :y ,846 :x ,"egdod otua" :txet {                    
+,} 58.0 :w ,58.0 :h ,591 :y ,305 :x ,"tob mia" :txet {                    
+,} 58.0 :w ,58.0 :h ,591 :y ,853 :x ,"toohsotua yar-x" :txet {                    
+[ = sgifnoc_nottub tsnoc                
+
+}{ )e( hctac }                
+}                    
+}                        
+}{ )e( hctac }                            
+;)rtp_ntb_esolc(retnioPetirw.)632(dda.jbo                                
+{ yrt                            
+{ ))(lluNsi.)rtp_ntb_esolc(rtp! && rtp_ntb_esolc( fi                        
+;)1 ,rtp_eman_ntb ,jbo(nottubemagdda_reniatnociugpord = rtp_ntb_esolc rav                        
+;)"nottub_esolc"(rtp_rts = rtp_eman_ntb rav                        
+{ )nottubemagdda_reniatnociugpord( fi                    
+{ yrt                
+
+}{ )e( hctac }                
+;))"4v srats elat"(rtp_cs ,)"txt_eltit"(rtp_rts ,jbo(txettes_pilceivom                    
+{ yrt                
+
+}                
+}{ )e( hctac }                    
+;)jbo ,ecnatsni_egats(dlihcdda_egats                        
+{ yrt                    
+{ ))(lluNsi.)ecnatsni_egats(rtp! && ecnatsni_egats( fi                
+
+}{ )e( hctac }                
+;)1(8Uetirw.)87(dda.jbo                    
+{ yrt                
+;)782 ,7.975 ,jbo(noitisop_tes_efas                
+;)1 ,1 ,jbo(ezis_tes_efas                
+
+}{ )e( hctac }                
+;)pilc_eivom ,jbo(rotc_pord                    
+;)]'retniop' ,'retniop'[ ,'diov' ,rdda_rotc_pord(noitcnuFevitaN wen = rotc_pord rav                    
+{ yrt                
+
+}{ )e( hctac }                
+;)1 ,pilc_eivom(potsdnaotog                    
+{ yrt                
+}                
+;llun nruter                    
+{ ))(lluNsi.)pilc_eivom(rtp || pilc_eivom!( fi                
+;))"pupop_pirts"(rtp_rts ,)"cs.iu/cs"(rtp_rts(pilceivomteg_elbatgnirts = pilc_eivom rav                
+
+}{ )e( hctac }                
+;)1 ,jbo(rotc_etirps                    
+{ yrt                
+}                
+;llun nruter                    
+{ ))(lluNsi.)jbo(rtp || jbo!( fi                
+;)004x0(colla.yromeM : )004x0(nf_collam ? nf_collam = jbo rav                
+{ yrt            
+{ )(pupop_pirts_etaerc noitcnuf        
+
+;][ = sriap_nottub_launam rav        
+;llun = pupop_tnerruc rav        
+
+}        
+}            
+;llun nruter                
+{ )e( hctac }            
+;ntb nruter                
+}                
+}{ )e( hctac }                    
+;)ntb ,ecnatsni_egats(dlihcdda_egats                        
+{ yrt                    
+{ ))(lluNsi.)ecnatsni_egats(rtp! && ecnatsni_egats( fi                
+
+;)1 ,1 ,ntb(ezis_tes_efas                
+;)025 ,05 ,ntb(noitisop_tes_efas                
+
+}{ )e( hctac }                
+;)1 ,)"unem dom"(rtp_cs ,ntb(evitan_txettes                    
+{ yrt                
+
+}                
+;llun nruter                    
+{ )e( hctac }                
+;)1 ,pilc ,ntb(nf_tini                    
+;)]'tni' ,'retniop' ,'retniop'[ ,'diov' ,rtp_tini(noitcnuFevitaN wen = nf_tini rav                    
+}                    
+;llun nruter                        
+{ ))(lluNsi.)rtp_tini(rtp || rtp_tini!( fi                    
+;)(retnioPdaer.)253(dda.rtpv = rtp_tini rav                    
+}                    
+;llun nruter                        
+{ ))(lluNsi.)rtpv(rtp || rtpv!( fi                    
+;)(retnioPdaer.ntb = rtpv rav                    
+{ yrt                
+
+}{ )e( hctac }                
+;)1 ,pilc(potsdnaotog                    
+{ yrt                
+}                
+;llun nruter                    
+{ ))(lluNsi.)pilc(rtp || pilc!( fi                
+;))"nottub_tixe_rotide_pam"(rtp_rts ,)"cs.iu/cs"(rtp_rts(pilceivomteg_elbatgnirts = pilc rav                
+
+;)ntb(rotc_nottubemag                
+}                
+;llun nruter                    
+{ ))(lluNsi.)ntb(rtp || ntb!( fi                
+;)445(colla.yromeM : )445(nf_collam ? nf_collam = ntb rav                
+{ yrt            
+{ )(nottub_tixe_etaerc noitcnuf        
+;llun = nottub_tixe rav        
+
+}        
+}            
+;llun nruter                
+{ )e( hctac }            
+;} txet :txet ,pilc :pilc ,ntb :ntb { nruter                
+}                
+}{ )e( hctac }                    
+;)ntb ,ecnatsni_egats(dlihcdda_egats                        
+{ yrt                    
+{ ))(lluNsi.)ecnatsni_egats(rtp! && ecnatsni_egats( fi                
+
+;)y ,x ,ntb(noitisop_tes_efas                
+;)1 || w ,1 || h ,ntb(ezis_tes_efas                
+
+}                
+}{ )e( hctac }                    
+}                        
+}                            
+;)tneidarg ,)txet(rtp_cs ,dleif_txet(txetdetarocedputes_dleiftxetdetaroced                                
+;)0 ,1 ,)txet(rtp_cs ,dleif_txet(yrassecenfielacsdnatxettes_replehpilceivom                                
+{ ))(lluNsi.)tneidarg(rtp! && tneidarg( fi                            
+;)1 ,)"nedloG"(rtp_cs(emanybtneidargrolocteg_selbatatadcigol = tneidarg rav                            
+{ ))(lluNsi.)dleif_txet(rtp! && dleif_txet( fi                        
+;))"txeT"(rtp_rts ,pilc(emanybdleiftxetteg_pilceivom = dleif_txet rav                        
+{ yrt                    
+{ )tneidarg_ylppa( fi                
+;)txet(sedulcni.]"egrahc otua" ,"egdod otua" ,"tob mia" ,"toohsotua yar-x"[ = tneidarg_ylppa rav                
+
+}{ )e( hctac }                
+;)1 ,)txet(rtp_cs ,ntb(evitan_txettes                    
+{ yrt                
+
+}                
+;llun nruter                    
+{ )e( hctac }                
+;)1 ,pilc ,ntb(nf_tini                    
+;)]'tni' ,'retniop' ,'retniop'[ ,'diov' ,rtp_tini(noitcnuFevitaN wen = nf_tini rav                    
+}                    
+;llun nruter                        
+{ ))(lluNsi.)rtp_tini(rtp || rtp_tini!( fi                    
+;)(retnioPdaer.)253(dda.rtpv = rtp_tini rav                    
+}                    
+;llun nruter                        
+{ ))(lluNsi.)rtpv(rtp || rtpv!( fi                    
+;)(retnioPdaer.ntb = rtpv rav                    
+{ yrt                
+
+}{ )e( hctac }                
+;)0 | emarf ,pilc(potsdnaotog                    
+{ yrt                
+}                
+;llun nruter                    
+{ ))(lluNsi.)pilc(rtp || pilc!( fi                
+;))eman_pilc(rtp_rts ,)"cs.iu/cs"(rtp_rts(pilceivomteg_elbatgnirts = pilc rav                
+
+;)ntb(rotc_nottubemag                
+}                
+;llun nruter                    
+{ ))(lluNsi.)ntb(rtp || ntb!( fi                
+;)445(colla.yromeM : )445(nf_collam ? nf_collam = ntb rav                
+{ yrt            
+{ )emarf ,w ,h ,y ,x ,txet ,eman_pilc(nottub_launam_etaerc noitcnuf        
+
+}        
+}{ )e( hctac }            
+;)w(taolFetirw.)82(dda.rtp_jbo                
+;)h(taolFetirw.)61(dda.rtp_jbo                
+}                
+;nruter                    
+{ ))(lluNsi.)rtp_jbo(rtp || rtp_jbo!( fi                
+{ yrt            
+{ )w ,h ,rtp_jbo(ezis_tes_efas noitcnuf        
+
+}        
+}{ )e( hctac }            
+;)y ,x ,rtp_jbo(yxdeppanslexiptes_tcejboyalpsid                
+}                
+;nruter                    
+{ ))(lluNsi.)rtp_jbo(rtp || rtp_jbo!( fi                
+{ yrt            
+{ )y ,x ,rtp_jbo(noitisop_tes_efas noitcnuf        
+
+}        
+}            
+;))s(gnirtS(8ftu_colla_efas nruter                
+{ )e( hctac }            
+;p nruter                
+;)))s(gnirtS(8ftu_colla_efas ,p(rotc_gnirts                
+;)04x0(nf_collam = p rav                
+{ yrt            
+}            
+;))s(gnirtS(8ftu_colla_efas nruter                
+{ )nf_collam!( fi            
+{ )s(rtp_cs noitcnuf        
+
+}        
+;))s(gnirtS(8ftu_colla_efas nruter            
+{ )s(rtp_rts noitcnuf        
+
+}{ )e( hctac }        
+;))rtp_ecnatsni_egats_ffo(dda.esab(retnioPdaer.yromeM = ecnatsni_egats            
+{ yrt        
+;llun = ecnatsni_egats rav        
+
+;)]'tni' ,'retniop' ,'retniop'[ ,'retniop' ,)4df1d4x0(dda.esab(noitcnuFevitaN wen = evitan_txettes rav        
+;)]'retniop'[ ,'diov' ,)8691d4x0(dda.esab(noitcnuFevitaN wen = rotc_nottubemag rav        
+
+}{ )e( hctac }        
+;)]'tni' ,'retniop' ,'retniop'[ ,'retniop' ,)nottubemagdda_reniatnociugpord_ffo(dda.esab(noitcnuFevitaN wen = nottubemagdda_reniatnociugpord            
+{ yrt        
+;llun = nottubemagdda_reniatnociugpord rav        
+;)]'tni' ,'tni' ,'retniop' ,'retniop'[ ,'diov' ,)yrassecenfielacsdnatxettes_replehpilceivom_ffo(dda.esab(noitcnuFevitaN wen = yrassecenfielacsdnatxettes_replehpilceivom rav        
+;)]'tni' ,'retniop'[ ,'retniop' ,)emanybtneidargrolocteg_selbatatadcigol_ffo(dda.esab(noitcnuFevitaN wen = emanybtneidargrolocteg_selbatatadcigol rav        
+;)]'retniop' ,'retniop' ,'retniop'[ ,'diov' ,)txetdetarocedputes_dleiftxetdetaroced_ffo(dda.esab(noitcnuFevitaN wen = txetdetarocedputes_dleiftxetdetaroced rav        
+;)]'retniop' ,'retniop'[ ,'retniop' ,)emanybdleiftxetteg_pilceivom_ffo(dda.esab(noitcnuFevitaN wen = emanybdleiftxetteg_pilceivom rav        
+;)rotc_reniatnociugpord_ffo(dda.esab = rdda_rotc_pord rav        
+;)]'retniop' ,'retniop' ,'retniop'[ ,'diov' ,)txettes_pilceivom_ffo(dda.esab(noitcnuFevitaN wen = txettes_pilceivom rav        
+;)]'tni' ,'retniop'[ ,'diov' ,)potsdnaotog_ffo(dda.esab(noitcnuFevitaN wen = potsdnaotog rav        
+;)]'tni' ,'retniop'[ ,'diov' ,)rotc_etirps_ffo(dda.esab(noitcnuFevitaN wen = rotc_etirps rav        
+;)]'taolf' ,'taolf' ,'retniop'[ ,'retniop' ,)yxdeppanslexiptes_tcejboyalpsid_ffo(dda.esab(noitcnuFevitaN wen = yxdeppanslexiptes_tcejboyalpsid rav        
+;)]'retniop' ,'retniop'[ ,'retniop' ,)dlihcdda_egats_ffo(dda.esab(noitcnuFevitaN wen = dlihcdda_egats rav        
+;)]'retniop' ,'retniop'[ ,'retniop' ,)gnirtsrts_tcurtsnoc_ffo(dda.esab(noitcnuFevitaN wen = rotc_gnirts rav        
+;)]'retniop' ,'retniop'[ ,'retniop' ,)pilceivomteg_elbatgnirts_ffo(dda.esab(noitcnuFevitaN wen = pilceivomteg_elbatgnirts rav        
+
+}        
+}{ )e( hctac }            
+;)]'tniu'[ ,'retniop' ,)'collam' ,eman.cbil(emaNyBtropxEteg.eludoM(noitcnuFevitaN wen = nf_collam                
+{ yrt            
+{ )cbil( fi        
+;llun = nf_collam rav        
+;)'6.os.cbil'(emaNyBeludoMdnif.ssecorP || )'os.cbil'(emaNyBeludoMdnif.ssecorP = cbil rav        
+
+}{ )e( hctac }        
+}            
+;)'xwr' ,ezis.dom ,esab.dom(tcetorp.yromeM                
+{ )ezis.dom && dom( fi            
+;)'os.gbil'(emaNyBeludoMdnif.ssecorP = dom rav            
+{ yrt        
+{ )esab(esab_htiw_partstoob noitcnuf    
+
+}    
+;)'os.gbil'(sserddAesaBdnif.eludoM nruter        
+{ )(esab_bil_teg noitcnuf    
+
+}    
+}        
+;)'0x0'(rtp nruter            
+{ )e( hctac }        
+;))s(gnirtS(gnirtS8ftUcolla.yromeM nruter            
+{ yrt        
+{ )s(8ftu_colla_efas noitcnuf    
+
+;}{ = setats_nottub rav    
+
+;9999 = y_neercs_ffo rav    
+;9999 = x_neercs_ffo rav    
+;002 = sm_ecnuobed rav    
+;051 = sm_llop rav    
+
+;43a068x0 = yrassecenfielacsdnatxettes_replehpilceivom_ffo rav    
+;0433f8x0 = emanybtneidargrolocteg_selbatatadcigol_ffo rav    
+;c748c4x0 = txetdetarocedputes_dleiftxetdetaroced_ffo rav    
+;4b2e9ax0 = emanybdleiftxetteg_pilceivom_ffo rav    
+;4ddecax0 = desserp_nottubmotsuc_ffo rav    
+;02f2d4x0 = nottubemagdda_reniatnociugpord_ffo rav    
+;87d2d4x0 = rotc_reniatnociugpord_ffo rav    
+;05cfcax0 = txettes_pilceivom_ffo rav    
+;c62d9ax0 = potsdnaotog_ffo rav    
+;41adaax0 = rotc_etirps_ffo rav    
+;4f789ax0 = yxdeppanslexiptes_tcejboyalpsid_ffo rav    
+;c6d6bax0 = dlihcdda_egats_ffo rav    
+;0050a01x0 = rtp_ecnatsni_egats_ffo rav    
+;84807ax0 = pilceivomteg_elbatgnirts_ffo rav    
+;06044cx0 = gnirtsrts_tcurtsnoc_ffo rav    
+stesffo //    
+{ )( noitcnuf(
+00202oyR@ iu elat //
